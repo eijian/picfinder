d03c2cf685e291883e9629846641363b9cde23bb
eijian <akagi.eiji.us@gmail.com>
2014-12-18 23:59:22 +0900

    update @office

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,37 @@
-
+--
+-- Main.hs
+--
 
 module Main where
 
+import System.Environment
 import System.Directory
 import Data.List.Split
 import Data.Char
 
+picext = "png"
 
+main :: IO ()
 main = do
-  fs <- getDirectoryContents "c:\\users\\s950142\\work"
-  putStrLn $ unlines $ filter isJpeg fs
+  ds <- getArgs
+  fs <- mapM getFileLists ds
+  putPics $ concat fs
+
+getFileLists :: FilePath -> IO [(String, String)]
+getFileLists f = do
+  fs <- getDirectoryContents f
+  return $ map (toFileInfo f) (filter isJpeg fs)
 
 isJpeg :: String -> Bool
-isJpeg f = if ext == "csv" then True else False
+isJpeg f = if ext == picext then True else False
   where
-  ext = last . splitOn "." f
+  ext = last $ splitOn "." f
 
+toFileInfo :: String -> String -> (String, String)
+toFileInfo d f = (d ++ "\\" ++ f, f)
+
+putPics :: [(String, String)] -> IO ()
+putPics [] = putStr ""
+putPics (p:ps) = do
+  putStrLn $ fst p
+  putPics ps
